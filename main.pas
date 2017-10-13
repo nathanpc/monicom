60a64690ba1247193518645a8c4d893382a47cf9
Nathan Campos <eeepc904@gmail.com>
2017-10-13 11:01:02 -0300

    Started working in the UI

--- main.pas
+++ main.pas
@@ -5,10 +5,34 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
+  ComCtrls, StdCtrls, ExtCtrls;
 
 type
+
+  { TfrmMain }
+
   TfrmMain = class(TForm)
+    btSend: TButton;
+    chkCRLF: TCheckBox;
+    mnuPortCustom: TMenuItem;
+    mnuHardwareFlowCtrl: TMenuItem;
+    mnuStopBits: TMenuItem;
+    mnuDataBits: TMenuItem;
+    mnuParity: TMenuItem;
+    mnuPort: TMenuItem;
+    mnuBaudRate: TMenuItem;
+    mnuExit: TMenuItem;
+    mnuSetup: TMenuItem;
+    panSendCommand: TPanel;
+    txtSend: TEdit;
+    mmoConsole: TMemo;
+    mnuHelp: TMenuItem;
+    mnuEdit: TMenuItem;
+    mnuFile: TMenuItem;
+    mnuMain: TMainMenu;
+    panSend: TPanel;
+    stbMain: TStatusBar;
   private
     { private declarations }
   public
