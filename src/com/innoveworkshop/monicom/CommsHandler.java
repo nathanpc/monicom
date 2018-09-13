b41c5adb44801167bcfc54439a4c6be6a4147ba6
Nathan Campos <eeepc904@gmail.com>
2018-09-12 22:13:46 -0300

    Implemented all the getters and setters in the serial helper class

--- src/com/innoveworkshop/monicom/CommsHandler.java
+++ src/com/innoveworkshop/monicom/CommsHandler.java
@@ -13,14 +13,39 @@ import sun.security.ssl.Debug;
  */
 public class CommsHandler {
     private String port;
+    private int baud;
+    private int parity;
+    private int data_bits;
+    private int stop_bits;
     private boolean connected;
 
     /**
-     * Creates a new CommsHandler object.
+     * Creates a new CommsHandler object with the default configuration.
+     * 
+     * Defaults:
+     *   - Baud Rate: 9600
+     *   - Parity: None
+     *   - Data Bits: 8
+     *   - Stop Bits: 1
      */
     public CommsHandler() {
         this.port = null;
+        this.baud = 9600;
+        this.parity = SerialPort.PARITY_NONE;
+        this.data_bits = SerialPort.DATABITS_8;
+        this.stop_bits = SerialPort.STOPBITS_1;
         this.connected = false;
+    }
+    
+    /**
+     * Creates a configuration string like "COM1 9600 8N1" for display.
+     * 
+     * @return Serial port configuration string.
+     */
+    public String getConfigurationString() {
+        String str = "";
+        // TODO: Create a configuration string like "COM1 9600 8N1" for the window title.
+        return str;
     }
     
     /**
@@ -63,6 +88,170 @@ public class CommsHandler {
      */
     public String getPort() {
         return this.port;
+    }
+    
+    /**
+     * Sets the serial baud rate.
+     * 
+     * @param baud_rate Baud rate.
+     * @return True if the baud rate value is valid.
+     */
+    public boolean setBaudRate(int baud_rate) {
+        if (baud_rate > 0) {
+            this.baud = baud_rate;
+            return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Gets the current baud rate.
+     * 
+     * @return Current baud rate.
+     */
+    public int getBaudRate() {
+        return this.baud;
+    }
+    
+    /**
+     * Sets the parity bit configuration.
+     * 
+     * @param parity Parity type as a single character: (N, O, E, M, S).
+     * @return True if it is a valid type of parity.
+     */
+    public boolean setParity(char parity) {
+        switch (parity) {
+            case 'N':
+                this.parity = SerialPort.PARITY_NONE;
+                return true;
+            case 'O':
+                this.parity = SerialPort.PARITY_ODD;
+                return true;
+            case 'E':
+                this.parity = SerialPort.PARITY_EVEN;
+                return true;
+            case 'M':
+                this.parity = SerialPort.PARITY_MARK;
+                return true;
+            case 'S':
+                this.parity = SerialPort.PARITY_SPACE;
+                return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Gets the parity bit configuration.
+     * 
+     * @return Parity type as a single character: (N, O, E, M, S). '-' in case of error.
+     */
+    public char getParity() {
+        switch (this.parity) {
+            case SerialPort.PARITY_NONE:
+                return 'N';
+            case SerialPort.PARITY_ODD:
+                return 'O';
+            case SerialPort.PARITY_EVEN:
+                return 'E';
+            case SerialPort.PARITY_MARK:
+                return 'M';
+            case SerialPort.PARITY_SPACE:
+                return 'S';
+        }
+        
+        return '-';
+    }
+    
+    /**
+     * Sets the amount of data bits in the serial transmission.
+     * 
+     * @param bits Amount of data bits.
+     * @return True if it is a valid number of bits.
+     */
+    public boolean setDataBits(int bits) {
+        switch (bits) {
+            case 5:
+                this.data_bits = SerialPort.DATABITS_5;
+                return true;
+            case 6:
+                this.data_bits = SerialPort.DATABITS_6;
+                return true;
+            case 7:
+                this.data_bits = SerialPort.DATABITS_7;
+                return true;
+            case 8:
+                this.data_bits = SerialPort.DATABITS_8;
+                return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Gets the amount of data bits.
+     * 
+     * @return Number of data bits. 0 if error.
+     */
+    public int getDataBits() {
+        switch (this.data_bits) {
+            case SerialPort.DATABITS_5:
+                return 5;
+            case SerialPort.DATABITS_6:
+                return 6;
+            case SerialPort.DATABITS_7:
+                return 7;
+            case SerialPort.DATABITS_8:
+                return 8;
+        }
+        
+        return 0;
+    }
+    
+    /**
+     * Sets the number of stop bits in the serial communication.
+     * 
+     * @param bits Number of stop bits.
+     * @return True if the number of stop bits is valid.
+     */
+    public boolean setStopBits(float bits) {
+        int ibits = (int)bits;
+        if (bits == 1.5) {
+            ibits = 15;
+        }
+        
+        switch (ibits) {
+            case 1:
+                this.stop_bits = SerialPort.STOPBITS_1;
+                return true;
+            case 15:
+                this.stop_bits = SerialPort.STOPBITS_1_5;
+                return true;
+            case 2:
+                this.stop_bits = SerialPort.STOPBITS_2;
+                return true;
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Gets the number of stop bits.
+     * 
+     * @return Number of stop bits. 0 if error.
+     */
+    public float getStopBits() {
+        switch (this.stop_bits) {
+            case SerialPort.STOPBITS_1:
+                return 1.0f;
+            case SerialPort.STOPBITS_1_5:
+                return 1.5f;
+            case SerialPort.STOPBITS_2:
+                return 2.0f;
+        }
+        
+        return 0.0f;
     }
     
     /**
